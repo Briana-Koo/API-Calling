13e854205779695164c7948e2d630c4cb14f1b25
Briana-Koo <RoyalBlueSnow@gmail.com>
2022-02-17 21:57:17 -0600

    Commit 1

---
+++ API Calling/Books.swift
@@ -0,0 +1,35 @@
+//
+//  Books.swift
+//  API Calling
+//
+//  Created by Student on 2/17/22.
+//
+
+import Foundation
+
+struct Book: Codable, Identifiable {
+    var id = UUID()
+    var author: String
+    var email: String
+    var title: String
+}
+
+class Api: ObservableObject{
+    
+    @Published var books = [Book]()
+    
+    func loadData(completion:@escaping ([Book]) -> ()) {
+        guard let url = URL(string: "https://training.xcelvations.com/temp/books.json") else {
+            print("Invalid url...")
+            return
+        }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            let books = try! JSONDecoder().decode ([Book].self, from: data!)
+            print(books)
+            DispatchQueue.main.async {
+                completion(books)
+            }
+        }
+        .resume()
+    }
+}
--- API Calling/ContentView.swift
+++ API Calling/ContentView.swift
@@ -8,9 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var books = [Book]()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(books) { book in
+        VStack(alignment: .leading) {
+        Text("\(book.title)")
+                    .font(.title)
+                    .foregroundColor(.red)
+                    .padding(.bottom)
+        HStack{
+                    Text("\(book.author)")
+                        .font(.body)
+                        .fontWeight(.bold)
+        Text("\(book.email)")
+                        .font(.body)
+                        .fontWeight(.semibold)
+                }
+                Spacer()
+            }
+        }
+            .onAppear() {
+                Api().loadData { (books) in
+                    self.books = books
+                }
+            }
+        .navigationTitle("Book List")
     }
 }
 
